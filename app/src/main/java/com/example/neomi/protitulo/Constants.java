621fada16676435dd818edea48ebec539016d3b0
neomi <neomirc@msn.com>
2019-07-21 14:58:35 -0400

    Se solucionan problemas del layout.

--- app/src/main/java/com/example/neomi/protitulo/Constants.java
+++ app/src/main/java/com/example/neomi/protitulo/Constants.java
@@ -1,9 +1,37 @@
 package com.example.neomi.protitulo;
 
+import android.os.Build;
+
 public class Constants {
     public static final String BROADCAST_DETECTED_ACTIVITY = "activity_intent";
 
     static final long DETECTION_INTERVAL_IN_MILLISECONDS = 10 * 1000;
 
     public static final int CONFIDENCE = 70;
+
+    // shared parameters based on the settings -- key is stored in preferences.xml
+    final static String PREF_WALKING_SPEED = "Walking_speed";
+    public final static String PREF_DEBUG_MODE = "Debug_mode";
+    public final static String PREF_TRIP_TYPE = "Trip_type";
+    final static String PREF_PLOT_SPEED = "Plot_speed";
+    final static String PREF_NUMBER_TRIP_ROWS = "Number_of_trip_rows";
+    public final static String PREF_NUMBER_TRIP_READINGS = "Number_of_trip_readings";
+    final static String PREF_WAKEUP_GPS = "wakeup_gps";
+    // constants for GPS Satellites
+    public final static String GPS_SATELLITES = "gps_satellites";
+    public final static String GPS_USED_SATELLITES = "gps_used_satellites";
+    public final static String GPS_SNR = "gps_snr";
+    public final static String GPS = "GPS";
+    final static String GPS_AZIMUTH = "gps_azimuth";
+    final static String GPS_ELEVATION = "gps_elevation";
+    final static String GPS_PRN = "gps_prn";
+    final static String GPS_TYPE = "gps_type";
+    final static String GPS_SATINFO = "gps_satinfo";
+    final static String GLONASS = "GLONASS";
+
+    // android versions
+    final static boolean preMarshmallow  =  Build.VERSION.SDK_INT < Build.VERSION_CODES.M; // < 23
+    final static boolean postMarshmallow =  Build.VERSION.SDK_INT >= Build.VERSION_CODES.M; // >= 23
+    final static boolean postNougat =  Build.VERSION.SDK_INT >= Build.VERSION_CODES.N; // >= 24
+
 }
---
+++ app/src/main/java/com/example/neomi/protitulo/Satellite.java
@@ -0,0 +1,138 @@
+package com.example.neomi.protitulo;
+import android.location.GnssStatus;
+
+public class Satellite {
+    private float azimuth;
+    float elevation;
+    private float snr;               // signal to noise ratio
+    private int prn;                    // pseudo random number
+    boolean used = false;
+    String type = "";               // GPS, Glonass, IRNSS, Galileo, Baidou
+
+    private boolean validSatellite = false;
+    private int signalStrength = 0;         // signal strength index
+    public double x = 0;                   // cartesian coordinates
+    public double y = 0;
+    public double z = 0;
+
+    @Override
+    public String toString() {
+        return "PRN: " + prn + " AZM: " + azimuth + " ALT: " + elevation + " SNR: " + snr +
+                " x: " + x  + " y: " + y + " z: " + z;
+    }
+
+    Satellite (float azimuth, float elevation, float snr, int prn, boolean used) {
+        this.azimuth = azimuth;
+        this.elevation = elevation;
+        this.snr = snr;
+        this.prn = prn;
+        this.used = used;
+        setType();
+
+    }
+
+    Satellite (float azimuth, float elevation, float snr, int prn, boolean used, int type) {
+        this.azimuth = azimuth;
+        this.elevation = elevation;
+        this.snr = snr;
+        this.prn = prn;
+        this.used = used;
+        switch (type) {
+            case GnssStatus.CONSTELLATION_GPS:
+                this.type = "GPS"; break;
+            case GnssStatus.CONSTELLATION_BEIDOU:
+                this.type = "BEIDOU"; break;
+            case GnssStatus.CONSTELLATION_GLONASS:
+                this.type = "GLONASS"; break;
+            case GnssStatus.CONSTELLATION_QZSS:
+                this.type = "QZSS"; break;
+            case GnssStatus.CONSTELLATION_GALILEO:
+                this.type = "Galileo"; break;
+            case GnssStatus.CONSTELLATION_SBAS:
+                this.type = "SBAS"; break;
+            default:
+                this.type = "DEFENSE";
+        }
+
+    }
+
+    public float getAzimuth() {
+        return azimuth;
+    }
+
+    public void setAzimuth(float azimuth) {
+        this.azimuth = azimuth;
+    }
+
+    public float getElevation() {
+        return elevation;
+    }
+
+    public void setElevation(float elevation) {
+        this.elevation = elevation;
+    }
+
+    public float getSnr() {
+        return snr;
+    }
+
+    public void setSnr(float snr) {
+        this.snr = snr;
+    }
+
+    public int getPrn() {
+        return prn;
+    }
+
+    public void setPrn(int prn) {
+        this.prn = prn;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType() {
+        if ( (0 <= prn) && (prn <= 64) )
+            this.type = "GPS";
+        else if ( (65 <= prn) && (prn <= 96))
+            this.type = "GLONASS";
+        else
+            this.type = "DEFENSE";
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public boolean isValidSatellite() {
+        return validSatellite;
+    }
+
+    /* return boolean if the satellite data is valid */
+    public void setValidSatellite() {
+        validSatellite = true;
+        if ( (azimuth < 0) || (azimuth > 360) ) validSatellite = false;
+        else if ( (elevation < 0) || (elevation > 90) ) validSatellite = false;
+        else if ( (prn < 0) || (prn > 1000) ) validSatellite = false;
+        else if ( (snr < 0) || (snr > 100)) validSatellite = false;
+        else if ( (Float.compare(azimuth, 0.0f) == 0) && (Float.compare(elevation, 0.0f) == 0) )
+            validSatellite = false;
+    }
+
+
+
+
+}
